9371c35f8f4c074c8c5943f9b4e6f693cef46c6f
Austin0613123 <163660797+Austin0613123@users.noreply.github.com>
2026-04-30 12:35:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ if submit_btn:
         p["期刊名"] = clean_input(d["期刊名"], prefix="，", wrap="《》")
         p["卷數"] = clean_input(d["卷數"], wrap="，")
         p["期數"] = clean_input(d["期數"], prefix="，第", suffix="期")
-        p["出版年份"] = clean_input(d["出版年份"], prefix="()")
+        p["出版年份"] = clean_input(d["出版年份"], prefix="(", suffix=")")
         p["頁數"] = clean_input(d["頁數"], prefix="，頁")
 
     elif source_type == "報紙":
